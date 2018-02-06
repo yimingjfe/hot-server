b02eca5f4b7c0d78784c335a69ff382686faaaba
Ming <527990618@163.com>
2018-02-06 23:56:02 +0800

    增加几个命令行功能

--- src/hot.js
+++ src/hot.js
@@ -11,17 +11,31 @@ const posthtml = require('posthtml')
 const ISHTMLASSET = /\.html$/
 
 // 增加指定端口功能
-const argv = require('yargs')
+const options = require('yargs')
   .option('d', {
     alias: 'dir',
     describe: '指定提供资源的目录',
     default: './'
   })
+  .options('p', {
+    alias: 'port',
+    describe: '指定对应的端口号',
+    default: 3000
+  })
+  .options('browser', {
+    alias: '-b',
+    describe: '指定打开的浏览器',
+  })
+  .options('o', {
+    alias: 'open',
+    describe: '是否打开浏览器',
+    default: false
+  })
   .argv;
 
 
-function start(argv){
-  let assetPath = argv.dir || './';
+function start(){
+  let assetPath = options.dir || './';
   console.log('assetPath', assetPath);
   if(!ISHTMLASSET.test(assetPath)){
     assetPath = path.resolve(assetPath, 'index.html')
@@ -37,7 +51,7 @@ function send404(req, res){
   }
 }
 
-// 增加关闭，删除script功能
+// 将增加和删除script的操作抽离出来
 async function startServer(dir){
   try {
     const file = path.resolve(dir, './index.html');
@@ -48,15 +62,28 @@ async function startServer(dir){
     const server = http.createServer((req, res) => {
       serve(req, res, send404(req, res));
     })
-    const port = await getPort(3000)
+    const port = await getPort(options.port)
   
     server.listen(port, () => {
-      opn(`http://localhost:${port}`)
-    }) 
+      if(options.o){
+        opn(`http://localhost:${port}`, {app: options.browser || ''})
+      }
+    })
+
+    server.on('error', () => {
+      console.error('error', error)
+    })
+
+
+    process.on('SIGINT', async() => {
+      await fs.writeFile(file, content);
+      process.exit(0);
+    })
   } catch (error) {
     console.error(error)
   }
 }
+
 
 function addClientScript(tree){
   tree.match({ tag: 'body' }, (node) => {
@@ -74,4 +101,4 @@ async function transformHtml(content){
   return newContent.html;
 }
 
-start(argv)
+start()
